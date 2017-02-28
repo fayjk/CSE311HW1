0afd5d8314a0e958dd0b43fe8417b6d2bf809e3c
fayjk <fayjk@miamioh.edu>
2017-02-27 21:58:21 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
 def add(a, b):
   return a + b;
   
-numOne, numTwo = input('Input two numbers to add')
-print ('result = ' , add(numOne,numTwo))
+
+test_add():
+  numOne, numTwo = input('Input two numbers to add')
+  print ('result = ' , add(numOne,numTwo))
+
+test_add()
